677d988cd717c9cfb19d277a6b602b019e4166ba
cindyuc <ccchenxinyu@gmail.com>
2022-05-14 21:49:42 -0400

    update

---
+++ ch02/Section2.1/type.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    unsigned u = 10, u2 = 42;
+    int i = 10, i2 = 42;
+
+    std::cout << i - u << std::endl;
+    std::cout << u - i << std::endl;
+
+    std::cout << '\n';
+    std::cout << "\tHi!\n";
+    std::cout << "3.14159L";
+    
+    return 0;
+}
