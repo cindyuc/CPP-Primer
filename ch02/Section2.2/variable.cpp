11d196b7ab89e7854f4bf805ab939cb61b1abf48
cindyuc <ccchenxinyu@gmail.com>
2022-05-15 02:55:54 -0400

    update 2.2

---
+++ ch02/Section2.2/variable.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "Sales_item.h"
+
+std::string global_str;
+int global_int;
+
+int main()
+{
+    int sum = 0, value, units_sold = 0;
+    Sales_item item;
+
+    std::string book("0-201-78345-X");
+    std::cout << book;
+
+    // list initialization
+    std::string empty;
+
+    int local_int;
+    std::string local_str;
+    std::cout << local_int;
+    std::cout << local_str;
+
+    int _;
+    double Double = 3.14;
+
+    return 0;
+    
+}
